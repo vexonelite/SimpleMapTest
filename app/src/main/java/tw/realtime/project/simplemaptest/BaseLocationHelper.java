18d962ea604378d204847165853da174a73bf63e
vexonelite <elite@realtime.tw>
2018-04-10 14:34:42 +0800

    adapt to upgrade of Android Studio v3.1

--- app/src/main/java/tw/realtime/project/simplemaptest/BaseLocationHelper.java
+++ app/src/main/java/tw/realtime/project/simplemaptest/BaseLocationHelper.java
@@ -8,7 +8,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Looper;
 import android.support.annotation.NonNull;
-import android.support.v4.content.PermissionChecker;
 import android.util.Log;
 
 import com.google.android.gms.common.api.ApiException;
